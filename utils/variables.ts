d9493b52dc24e392b070a2379a824529762e947a
mems2001 <mems2001code@gmail.com>
2026-03-04 19:38:15 -0500

    test: numbers wheel for games 4

--- utils/variables.ts
+++ utils/variables.ts
@@ -4,17 +4,17 @@ interface Urls {
     share_link_prefix: string
 }
 
-// const urls:Urls = {
-//     url_prefix : 'https://sudoku-db-ip9b.onrender.com',
-//     socket_url: 'https://sudoku-db-ip9b.onrender.com',
-//     share_link_prefix: 'https://sudoku21.netlify.app/'
-// }
+const urls:Urls = {
+    url_prefix : 'https://sudoku-db-ip9b.onrender.com',
+    socket_url: 'https://sudoku-db-ip9b.onrender.com',
+    share_link_prefix: 'https://sudoku21.netlify.app/'
+}
 
 // LOCAL
-const urls:Urls = {
-    url_prefix : 'http://localhost:443',
-    socket_url: 'http://localhost:443',
-    share_link_prefix: 'http://localhost:5173'
-}
+// const urls:Urls = {
+//     url_prefix : 'http://localhost:443',
+//     socket_url: 'http://localhost:443',
+//     share_link_prefix: 'http://localhost:5173'
+// }
 
 export default urls
