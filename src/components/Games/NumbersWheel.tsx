86c68542f774f48fa40b9e19442cd81a6346e51c
mems2001 <mems2001code@gmail.com>
2026-03-04 19:14:24 -0500

    test: numbers wheel for games 3

--- src/components/Games/NumbersWheel.tsx
+++ src/components/Games/NumbersWheel.tsx
@@ -8,7 +8,7 @@ interface WheelProps {
 
 const NumbersWheel:React.FC<WheelProps> = ({ currentFocused}) => {
     return (
-        <motion.div className="numbers-wheel" {...NumbersWheelProps}>
+        <motion.div className="numbers-wheel" style={{top: `${(parseInt(currentFocused![0]) * (2/3) * 100)/8}%`, left: `${(parseInt(currentFocused![1]) * (2/3) * 100)/8}%`}} {...NumbersWheelProps}>
             {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
                 <div key={number} className="number">{number} {currentFocused}</div>
             ))}
